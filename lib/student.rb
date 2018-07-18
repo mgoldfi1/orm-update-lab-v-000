802ec020627f745241251895ce22c353091c9764
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-18 14:09:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,11 @@
 require_relative "../config/environment.rb"
-
+attr_accessor :id, :name, :grade
 class Student
 
 def initialize(id=nil,name,grade)
-  @id = id 
-  @name = name 
-  @grade = grade 
+  @id = id
+  @name = name
+  @grade = grade
 end
 
 
