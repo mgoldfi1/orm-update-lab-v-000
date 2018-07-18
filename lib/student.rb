aae0aabd84b89739686aba4f867874bc0738c6ae
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-18 15:05:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,10 @@ attr_accessor :id, :name, :grade
     student = Student.new(row[0],row[1],row[2])
     end
 
-
+  def self.find_by_name(name)
+    sql = "SELECT name, grade  FROM students WHERE name = ?"
+    DB[:conn].execute(sql,name)
+  end
 
 
 
